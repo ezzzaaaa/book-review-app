9e4a0f72916071c16a594d8e9c4b7fd6d18ca073
ezzzaaaa <ezzaaa34@gmail.com>
2026-04-30 12:28:29 -0400

    Completed Task 11 - Async/Await ISBN book details

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -46,11 +46,26 @@ public_users.get('/', async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  return res.status(200).send(JSON.stringify(books[isbn], null, 4));
+public_users.get('/isbn/:isbn', async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
+
+    const getBookByISBN = (isbn) =>
+      new Promise((resolve) => {
+        resolve(books[isbn]);
+      });
+
+    const book = await getBookByISBN(isbn);
+
+    if (book) {
+      return res.status(200).json(book);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching book" });
+  }
 });
-  
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
