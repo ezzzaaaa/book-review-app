4d34895b2ceb9befe6504f73f869bf56c263db41
ezzzaaaa <ezzaaa34@gmail.com>
2026-04-30 11:52:16 -0400

    Completed Task 9 - Delete Book Review API

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -75,6 +75,41 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   }
 });
 
+// Task 9 - DELETE REVIEW
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+   const isbn = req.params.isbn;
+
+   const authHeader = req.headers.authorization;
+
+   if (!authHeader) {
+     return res.status(403).json({ message: "User not logged in" });
+   }
+
+   const token = authHeader.split(" ")[1];
+
+   try {
+     const user = jwt.verify(token, "fingerprint_customer");
+
+     if (!books[isbn]) {
+       return res.status(404).json({ message: "Book not found" });
+     }
+
+     if (!books[isbn].reviews) {
+       return res.status(404).json({ message: "No reviews found" });
+     }
+
+     delete books[isbn].reviews[user.username];
+
+     return res.status(200).json({
+       message: "Review deleted successfully",
+       reviews: books[isbn].reviews
+     });
+
+   } catch (err) {
+     return res.status(403).json({ message: "Invalid token" });
+   }
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
